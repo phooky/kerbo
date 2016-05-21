69d868851a3df3b5a0cd46fde186656a60783216
Adam Mayer <phooky@gmail.com>
2016-05-21 14:37:27 -0400

    construct from iterator

--- src/img_proc/mod.rs
+++ src/img_proc/mod.rs
@@ -4,14 +4,24 @@ use std::clone::Clone;
 
 type Pixel = u8;
 
-struct MemImage<P> {
+pub struct MemImage<P> {
     size : (usize, usize),
     data : Vec<P>,
     stride : usize,
 }
 
+impl<P> MemImage<P> {
+    pub fn from_iterator<I>(i : I, sz : (usize, usize) ) -> MemImage<P>
+        where I : Iterator<Item=P>
+    {
+        let (_, stride) = sz;
+        let v : Vec<P> = i.collect();
+        MemImage{ size : sz, stride : stride, data : v }
+    }
+}
+
 impl<P : Sub<P,Output=P> + Clone > MemImage<P> {
-    fn subtract(&mut self, other : MemImage<P>) {
+    pub fn subtract(&mut self, other : MemImage<P>) {
         let mut o = other.data.iter();
         for p in self.data.iter_mut() {
             match o.next() {
@@ -36,3 +46,14 @@ fn test_image_sub() {
     a.subtract(b);
     assert_eq!( a.data, vec![8,6,4,4,2,0,0,-2,-4]);
 }
+
+#[test]
+fn test_from_iterator() {
+    let v : Vec<u8> = vec![1,2,3,4,5,6,7,8,9];
+    let i = v.into_iter();
+    let m = MemImage::from_iterator(i,(3,3));
+    assert_eq!(m.data, vec![1,2,3,4,5,6,7,8,9]);
+    assert_eq!(m.stride, 3);
+    assert_eq!(m.size, (3,3));
+}
+        
